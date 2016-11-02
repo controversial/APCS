4821caf8a4c1a015970fcb1b9b4f7ddc50fe1d71
Luke Deen Taylor <ldt@deentaylor.com>
2016-11-01 22:29:07 -0400

    Create in3050.java

---
+++ codingbat/Warmup-1/in3050.java
@@ -0,0 +1,5 @@
+public boolean in3050(int a, int b) {
+  boolean in3040 = a >= 30 && a <= 40 && b >= 30 && b <= 40;
+  boolean in4050 = a >= 40 && a <= 50 && b >= 40 && b <= 50;
+  return in3040 || in4050;
+}
