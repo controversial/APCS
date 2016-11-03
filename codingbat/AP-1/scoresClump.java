2d6d3c544608b9b88294512e264f4c2ebe2078e4
Luke Deen Taylor <ldt@deentaylor.com>
2016-11-03 16:35:07 -0400

    Create scoresClump.java

---
+++ codingbat/AP-1/scoresClump.java
@@ -0,0 +1,6 @@
+public boolean scoresClump(int[] scores) {
+  for (int i=0; i<scores.length-2; i++) {
+    if (Math.abs(scores[i+1] - scores[i]) <= 2 && Math.abs(scores[i+2] - scores[i]) <= 2) return true;
+  }
+  return false;
+}
