219013265b955be2faaa497e1bac8f7be31bbab4
Luke Deen Taylor <ldt@deentaylor.com>
2016-11-05 13:12:02 -0400

    Create luckySum.java

---
+++ codingbat/Logic-2/luckySum.java
@@ -0,0 +1,7 @@
+public int luckySum(int a, int b, int c) {
+  int sum = 0;
+  if (a != 13) {sum += a;} else {return sum;}
+  if (b != 13) {sum += b;} else {return sum;}
+  if (c != 13) sum += c;
+  return sum;
+}
