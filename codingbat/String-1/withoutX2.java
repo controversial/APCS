dd265215bf288635d969aec7dc3a80b0babf7b89
Luke Deen Taylor <ldt@deentaylor.com>
2017-01-05 19:05:25 -0500

    Create withoutX2.java

---
+++ codingbat/String-1/withoutX2.java
@@ -0,0 +1,11 @@
+public String withoutX2(String str) {
+  if (str.length() < 2) return str.equals("x") ? "" : str;
+
+  if (str.substring(0, 2).equals("xx"))
+    return str.substring(2);
+  else if (str.charAt(0) == 'x')
+    return str.substring(1);
+  else if (str.charAt(1) == 'x')
+    return str.substring(0, 1) + str.substring(2);
+  return str;
+}
