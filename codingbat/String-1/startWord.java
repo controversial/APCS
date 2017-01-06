d93c4ab7bc2b10c502c38814fabfaf369dfeba6a
Luke Deen Taylor <ldt@deentaylor.com>
2017-01-05 19:00:28 -0500

    Create startWord.java

---
+++ codingbat/String-1/startWord.java
@@ -0,0 +1,6 @@
+public String startWord(String str, String word) {
+  if (word.length() > str.length()) return "";
+  if (word.substring(1).equals(str.substring(1, word.length())))
+    return str.substring(0, word.length());
+  return "";
+}
