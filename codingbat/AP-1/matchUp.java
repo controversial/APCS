96c4f2cccf2d4b6ffc6c29631740227d1b0b6c4d
Luke Deen Taylor <ldt@deentaylor.com>
2017-03-08 14:57:23 -0500

    Create matchUp.java

---
+++ codingbat/AP-1/matchUp.java
@@ -0,0 +1,9 @@
+public int matchUp(String[] a, String[] b) {
+  int count = 0;
+  for (int i = 0; i < a.length; i++) {
+    if (a[i].length() > 0 && b[i].length() > 0 && a[i].substring(0, 1).equals(b[i].substring(0, 1))) {
+      count++;
+    }
+  }
+  return count;
+}
