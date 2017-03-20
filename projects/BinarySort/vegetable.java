e6c8394e99cc80e1bab1cef6ab14e396d22dfa42
Luke Deen Taylor <ldt@deentaylor.com>
2017-03-20 14:43:42 -0400

    Create vegetable.java

---
+++ projects/BinarySort/vegetable.java
@@ -0,0 +1,17 @@
+/**
+ * This class is complete
+ */
+public class vegetable
+{
+    private int number;
+    private String name;
+
+    public vegetable(int num, String n)
+    {
+        number = num;
+        name = n;
+    }
+
+    public int getNum() { return number; }
+    public String getName() { return name; }
+}
